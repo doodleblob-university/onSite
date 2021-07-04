7605bc7ec55b68cfee4d35ac3eaa7286e72a5c01
barryc2 <barryc2@uni.coventry.ac.uk>
2021-07-04 01:50:28 +0100

    Created activities

---
+++ app/src/main/java/com/example/cb300cem/LoadApp.java
@@ -0,0 +1,30 @@
+package com.example.cb300cem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class LoadApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getActionBar().hide();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.load);
+
+        
+
+
+        Intent intent = new Intent(this, Main.class);
+        startActivity(intent);
+
+
+
+
+        finish(); // close loading activity
+    }
+}
+
---
+++ app/src/main/java/com/example/cb300cem/Login.java
@@ -0,0 +1,14 @@
+package com.example.cb300cem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
